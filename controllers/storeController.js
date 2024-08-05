aedff5942e8e87c01d2108b5f0cf52ee680a7be1
DiegoCoura <diegogomes.10.9@gmail.com>
2024-08-05 19:00:06 -0300

    feat: add index controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,6 +2,11 @@ const asyncHandler = require("express-async-handler")
 const db = require("../db/queries");
 
 exports.index = asyncHandler(async(req,res,next)=>{
-    // const allProducts = await db.getAllProducts();
-    res.render("index")
+    const allCategories = await db.getAllCategories();
+    const allProducts = await db.getAllProducts();
+    
+    res.render("index", {
+        categories: allCategories,
+        products: allProducts
+    })
 })
