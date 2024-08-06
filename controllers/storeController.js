cd641b44297659aaf9e04f7999d1d248cc06f4b7
DiegoCoura <diegogomes.10.9@gmail.com>
2024-08-05 22:38:42 -0300

    feat: category filter

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,22 @@
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 
-exports.index = asyncHandler(async(req,res,next)=>{
-    const allCategories = await db.getAllCategories();
-    const allProducts = await db.getAllProducts();
-    
-    res.render("index", {
-        categories: allCategories,
-        products: allProducts
-    })
-})
+exports.index = asyncHandler(async (req, res, next) => {
+  const allCategories = await db.getAllCategories();
+  const allProducts = await db.getAllProducts();
+
+  res.render("index", {
+    categories: allCategories,
+    products: allProducts,
+  });
+});
+
+exports.byCategory = asyncHandler(async (req, res, next) => {
+  const allCategories = await db.getAllCategories();
+  const categoryProducts = await db.getCategoryById(req.params.id);
+
+  res.render("index", {
+    categories: allCategories,
+    products: categoryProducts,
+  });
+});
