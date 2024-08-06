7a626986251df629f225beb7cdc0cb179a0337ff
DiegoCoura <diegogomes.10.9@gmail.com>
2024-08-05 23:52:21 -0300

    feat: product_detail controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -20,3 +20,17 @@ exports.byCategory = asyncHandler(async (req, res, next) => {
     products: categoryProducts,
   });
 });
+
+exports.product_detail = asyncHandler(async (req, res, next) => {
+  const product = await db.getProductById(req.params.id);
+  
+  if (!product.length) {
+    const err = new Error("Product Not Found!");
+    err.status = 404;    
+    return next(err);
+  } else {
+    res.render("productDetails", {
+      product: product[0],
+    });
+  }
+});
