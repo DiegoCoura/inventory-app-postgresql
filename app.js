6096ed0c42a2b3db54291c220014a21914d41b9a
DiegoCoura <diegogomes.10.9@gmail.com>
2024-08-07 01:43:23 -0300

    fix: urlencoded set false

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const app = express();
 require("dotenv").config();
 
 const storeRouter = require("./routes/storeRouter");
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + "/public")));
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
