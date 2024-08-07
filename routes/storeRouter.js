ce00339b444112b5d8ed2562a3c8ef518d615df9
DiegoCoura <diegogomes.10.9@gmail.com>
2024-08-07 20:10:35 -0300

    feat: product add, delete and edit routes

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -5,13 +5,18 @@ const storeRouter = Router();
 
 storeRouter.get("/", storeController.index);
 
-storeRouter.get("/product/edit/:id", storeController.product_update_get)
+storeRouter.get("/product/new", storeController.add_product_get);
 
-storeRouter.post("/product/edit/:id", storeController.product_update_post)
+storeRouter.post("/product/new", storeController.add_product_post);
+
+storeRouter.get("/product/edit/:id", storeController.product_update_get);
+
+storeRouter.post("/product/edit/:id", storeController.product_update_post);
+
+storeRouter.post("/product/delete/:id", storeController.delete_product);
 
 storeRouter.get("/category/:id", storeController.byCategory);
 
 storeRouter.get("/product/:id", storeController.product_detail);
-
 
 module.exports = storeRouter;
