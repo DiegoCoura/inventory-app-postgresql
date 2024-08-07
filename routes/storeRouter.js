f154a46f76791031d517df500a8821ca038701c0
DiegoCoura <diegogomes.10.9@gmail.com>
2024-08-07 01:51:58 -0300

    feat: update routes

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -5,8 +5,13 @@ const storeRouter = Router();
 
 storeRouter.get("/", storeController.index);
 
+storeRouter.get("/product/edit/:id", storeController.product_update_get)
+
+storeRouter.post("/product/edit/:id", storeController.product_update_post)
+
 storeRouter.get("/category/:id", storeController.byCategory);
 
 storeRouter.get("/product/:id", storeController.product_detail);
+
 
 module.exports = storeRouter;
