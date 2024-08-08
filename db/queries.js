3637109e882e64c3e8b1ba6fc024fa9f17a217d0
DiegoCoura <diegogomes.10.9@gmail.com>
2024-08-07 23:47:41 -0300

    feat: category queries

--- db/queries.js
+++ db/queries.js
@@ -29,20 +29,6 @@ exports.addNewProduct = async (productInfos) => {
   return rows;
 };
 
-exports.getAllCategories = async () => {
-  const { rows } = await pool.query("SELECT * FROM categories");
-
-  return rows;
-};
-
-exports.getCategoryById = async (categoryId) => {
-  const { rows } = await pool.query(
-    "SELECT * FROM products WHERE category_id = ($1)",
-    [categoryId]
-  );
-  return rows;
-};
-
 exports.updateProduct = async (newInfos) => {
   await pool.query(
     "UPDATE products SET product_name = ($1), description = ($2), category_id = ($3), price = ($4), image = ($5), quantity = ($6)  WHERE product_id = ($7)",
@@ -64,6 +50,36 @@ exports.deleteProduct = async (productId) => {
     "DELETE FROM products WHERE product_id = ($1) RETURNING product_id",
     [productId]
   );
-  console.log("query: ", rows);
+  return rows;
+};
+
+exports.getAllCategories = async () => {
+  const { rows } = await pool.query("SELECT * FROM categories");
+
+  return rows;
+};
+
+exports.getCategoryById = async (categoryId) => {
+  const { rows } = await pool.query(
+    "SELECT * FROM products WHERE category_id = ($1)",
+    [categoryId]
+  );
+  return rows;
+};
+
+exports.addCategory = async (categoryName) => {
+  const { rows } = await pool.query(
+    "INSERT INTO categories (category_name) VALUES ($1) RETURNING *",
+    [categoryName]
+  );
+
+  return rows;
+};
+
+exports.deleteCategory = async (categoryId) => {
+  const { rows } = await pool.query(
+    "DELETE FROM categories WHERE category_id = ($1) RETURNING *",
+    [categoryId]
+  );
   return rows;
 };
